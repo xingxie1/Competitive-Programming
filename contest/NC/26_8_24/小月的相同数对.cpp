99be10d75cf5415eec3f6446fb5ec83af8b9bf92
xingxie <1483281343@qq.com>
2026-08-24 10:56:52 +0800

    26.8.23

---
+++ contest/NC/26_8_24/小月的相同数对.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+using i128 = __int128_t;
+using vt = vector<int>;
+using vd = vector<double>;
+using vll = vector<long long>;
+using vvt = vector<vector<int>>;
+using vvd = vector<vector<double>>;
+using vvll = vector<vector<long long>>;
+using vvvt = vector<vector<vector<int>>>;
+using vvvll = vector<vector<vector<long long>>>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double,double>;
+using vpii = vector<pair<int,int>>;
+using vpll = vector<pair<ll,ll>>;
+using vpdd = vector<pair<double,double>>;
+using vvpii = vector<vector<pair<int,int>>>;
+using vvpll = vector<vector<pair<ll,ll>>>;
+using tri = tuple<int,int,int>;
+using trl = tuple<ll,ll,ll>;
+using vtri = vector<tuple<int,int,int>>;
+using vtrl = vector<tuple<ll,ll,ll>>;
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define fi first
+#define se second
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define mset multiset
+#define endl '\n'
+//const int MOD = 998244353;
+//const int MOD = (int)1e9+7;
+
+void solve()
+{
+    ll n,k;
+    cin >> n >> k;
+    vt a(n);
+    for (int i = 0;i < n;i++) cin >> a[i];
+    int l = 0,r = 0;
+    ll sum = 0;
+    int ans = INT_MAX / 2;
+    map<int,ll> cnt;
+    while (r < n)
+    {
+        sum += cnt[a[r]];
+        cnt[a[r]]++;
+        while (sum >= k)
+        {
+            // cout << l << " " << r << endl;
+            ans = min(ans,r - l + 1);
+            cnt[a[l]]--;
+            sum -= cnt[a[l]];
+            l++;
+        }
+        r++;
+    }
+    if (ans == INT_MAX / 2) cout << -1 << endl;
+    else cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << fixed << setprecision(15);
+    int _ = 1;
+    // cin >> _;
+    while (_ --) solve();
+
+    return 0;
+}
