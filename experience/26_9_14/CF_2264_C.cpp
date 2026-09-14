1f4bb547072286239b755c414b57b0b448e5d223
xingxie <1483281343@qq.com>
2026-09-14 23:03:54 +0800

    26.9.14

---
+++ experience/26_9_14/CF_2264_C.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+using i128 = __int128_t;
+using vt = vector<int>;
+using vd = vector<double>;
+using vll = vector<long long>;
+using vvt = vector<vector<int>>;
+using vvd = vector<vector<double>>;
+using vvll = vector<vector<long long>>;
+using vvvt = vector<vector<vector<int>>>;
+using vvvll = vector<vector<vector<long long>>>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double,double>;
+using vpii = vector<pair<int,int>>;
+using vpll = vector<pair<ll,ll>>;
+using vpdd = vector<pair<double,double>>;
+using vvpii = vector<vector<pair<int,int>>>;
+using vvpll = vector<vector<pair<ll,ll>>>;
+using tri = tuple<int,int,int>;
+using trl = tuple<ll,ll,ll>;
+using vtri = vector<tuple<int,int,int>>;
+using vtrl = vector<tuple<ll,ll,ll>>;
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define fi first
+#define se second
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define mset multiset
+#define endl '\n'
+const int MOD = 998244353;
+//const int MOD = (int)1e9+7;
+
+const int MX = 200001;
+ll F[MX]; // F[i] = i!
+ll INV_F[MX]; // INV_F[i] = i!^-1 = qpow(i!, MOD-2)
+
+ll qpow(ll a, ll b) 
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1) res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+auto init = [] 
+{
+    F[0] = 1;
+    for (int i = 1; i < MX; i++) 
+    {
+        F[i] = F[i - 1] * i % MOD;
+    }
+
+    INV_F[MX - 1] = qpow(F[MX - 1], MOD - 2);
+    for (int i = MX - 1; i; i--) 
+    {
+        INV_F[i - 1] = INV_F[i] * i % MOD;
+    }
+    return 0;
+}();
+
+// 从 n 个数中选 m 个数的方案数
+ll comb(int n, int m) 
+{
+    return m < 0 || m > n ? 0 : F[n] * INV_F[m] % MOD * INV_F[n - m] % MOD;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vll a(n);
+    for (int i = 0;i < n;i++) cin >> a[i];
+    ranges::sort(a);
+    vll pre(n + 1,1),suf(n + 1,1),sum(n + 1);
+    for (int i = 0;i < n;i++) pre[i + 1] = pre[i] * (n - i - 1) % MOD;
+    for (int i = n - 2;i >= 0;i--) suf[i] = suf[i + 1] * (n - i - 1) % MOD;
+    for (int i = n - 1;i >= 0;i--) sum[i] = sum[i + 1] + a[i];
+    ll ans = 0;
+    for (int i = 0;i < n - 1;i++)
+    {
+        ll s = sum[i + 1] - a[i] * (n - i - 1);
+        s %= MOD;
+        ll w = pre[i] * suf[i + 1] % MOD;
+        ans = (ans + w * s) % MOD;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << fixed << setprecision(15);
+    int _ = 1;
+    cin >> _;
+    while (_ --) solve();
+
+    return 0;
+}
